f15469a6cbbda58d039fd5b371b4a923b758a542
Aolderr18 <Aolderr18@gmail.com>
2016-07-08 22:30:19 -0500

    Update Point3DSpace.cpp

--- Point3DSpace.cpp
+++ Point3DSpace.cpp
@@ -1,43 +1,43 @@
 #include "Point3DSpace.h"
 
-Point3DSpace::Point3DSpace()
+Point3DSpace::Point3DSpace(void)
 {
     // Default constructor
 }
 
-Point3DSpace::Point3DSpace(BigNumber X_Coordinate, BigNumber Y_Coordinate, BigNumber Z_Coordinate)
+Point3DSpace::Point3DSpace(AccurateNumber X_Coordinate, AccurateNumber Y_Coordinate, AccurateNumber Z_Coordinate)
 {
     setX_Coordinate(X_Coordinate);
     setY_Coordinate(Y_Coordinate);
     setZ_Coordinate(Z_Coordinate);
 }
 
-BigNumber Point3DSpace::getX_Coordinate()
+AccurateNumber Point3DSpace::getX_Coordinate(void)
 {
     return X_Coordinate;
 }
 
-BigNumber Point3DSpace::getY_Coordinate()
+AccurateNumber Point3DSpace::getY_Coordinate(void)
 {
     return Y_Coordinate;
 }
 
-BigNumber Point3DSpace::getZ_Coordinate()
+AccurateNumber Point3DSpace::getZ_Coordinate(void)
 {
     return Z_Coordinate;
 }
 
-void Point3DSpace::setX_Coordinate(BigNumber X_Coordinate)
+void Point3DSpace::setX_Coordinate(AccurateNumber X_Coordinate)
 {
-    this->X_Coordinate = X_Coordinate;
+    Point3DSpace::X_Coordinate = X_Coordinate;
 }
 
-void Point3DSpace::setY_Coordinate(BigNumber Y_Coordinate)
+void Point3DSpace::setY_Coordinate(AccurateNumber Y_Coordinate)
 {
-    this->Y_Coordinate = Y_Coordinate;
+    Point3DSpace::Y_Coordinate = Y_Coordinate;
 }
 
-void Point3DSpace::setZ_Coordinate(BigNumber Z_Coordinate)
+void Point3DSpace::setZ_Coordinate(AccurateNumber Z_Coordinate)
 {
-    this->Z_Coordinate = Z_Coordinate;
+    Point3DSpace::Z_Coordinate = Z_Coordinate;
 }
