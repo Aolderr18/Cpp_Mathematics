d676d62705eb5c8881faedf8e3fee759269185c9
Aolderr18 <Aolderr18@gmail.com>
2016-07-08 22:27:24 -0500

    Update LineSegment3DSpace.cpp

--- LineSegment3DSpace.cpp
+++ LineSegment3DSpace.cpp
@@ -1,6 +1,6 @@
 #include "LineSegment3DSpace.h"
 
-LineSegment3DSpace::LineSegment3DSpace()
+LineSegment3DSpace::LineSegment3DSpace(void)
 {
     // Default constructor
 }
@@ -11,29 +11,29 @@ LineSegment3DSpace::LineSegment3DSpace(Point3DSpace start, Point3DSpace end)
     setEnd(end);
 }
 
-BigNumber LineSegment3DSpace::segmentLength()
+AccurateNumber LineSegment3DSpace::segmentLength(void)
 {
     // The following uses the Pythagorean theorem to compute the distance between the end point and the start point.
-    BigNumber segmentLength = ((end.getX_Coordinate().difference(start.getX_Coordinate())).sum(end.getY_Coordinate().difference(start.getY_Coordinate())).sum(end.getZ_Coordinate().difference(start.getZ_Coordinate()))).root(2);
+    AccurateNumber segmentLength = ((end.getX_Coordinate().difference(start.getX_Coordinate())).sum(end.getY_Coordinate().difference(start.getY_Coordinate())).sum(end.getZ_Coordinate().difference(start.getZ_Coordinate()))).root(2);
     return segmentLength;
 }
 
-Point3DSpace LineSegment3DSpace::getStart()
+Point3DSpace LineSegment3DSpace::getStart(void)
 {
     return start;
 }
 
-Point3DSpace LineSegment3DSpace::getEnd()
+Point3DSpace LineSegment3DSpace::getEnd(void)
 {
     return end;
 }
 
 void LineSegment3DSpace::setStart(Point3DSpace start)
 {
-    this->start = start;
+    LineSegment3DSpace::start = start;
 }
 
 void LineSegment3DSpace::setEnd(Point3DSpace end)
 {
-    this->end = end;
+    LineSegment3DSpace::end = end;
 }
